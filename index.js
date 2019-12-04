8ddca6accdf3c48fc0de5eccdb23e0bf2d34aa56
Julio Cesar Aparecido Gaiotto <julio.gaiotto@totvs.com.br>
2019-12-04 16:34:30 -0300

    criando middlewares

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const server = express();
 
 server.use(express.json());
 
+// middleware local (pode ser chamado em quanlaquer rota)
+function checkUserExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({error: 'User name is required'});
+  }
+  return next();
+}
+
+// Criando um middleware global (será executado antes de bater na rota chamada)
+server.use((req, res, next) => {
+  console.log(`Método: ${req.method}; URL: ${req.url}`);
+  return next();
+})
+
 // Query params = ?teste=1
 server.get('/users', (req, res) => {
   const nome = req.query.nome;
@@ -17,7 +31,7 @@ server.get('/users/:id', (req, res) => {
 });
 
 // Request body = { "nome": "Julio", "email": "teste@gmail.com" }
-server.post('/users', (req, res) => {
+server.post('/users', checkUserExists, (req, res) => {
   const { name, email } = req.body;
   res.json({
     "Nome": name,
